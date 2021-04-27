e48cb14eef2d8feab27b72d83bea76abec6f6490
giusmili <giusmili67@gmail.com>
2021-04-27 15:27:27 +0200

    menu final

--- js/app-menu-bar.js
+++ js/app-menu-bar.js
@@ -5,15 +5,39 @@ $(document).ready(()=>{ /* function main */
             date : new Date().getFullYear(),
            
     });
+    let header = $("header"); /* for toggleClass */
+    header.on("click",function(){
+        $(this).toggleClass("bg-header-add");
+        
+
+    })
+
     $("<p>").prependTo('footer');
     $("p").html("&copy; - MIT - "+msg[0].date);
 
     /* events */
+    //console.log($(".inner-header").next(".navigation:nth-of-type(1)"));
+    $(".inner-header:first-child").addClass("bg-color") /* si on veut un menu ouvert par défaut */
     $(".inner-header").next(".navigation").hide();
+    $(".inner-header").next(".navigation:nth-of-type(1)").css({'display':'block'});
+    
     $(".inner-header").on("mousedown",function(){
         //condition
+        if($(this).next(".navigation").is(":hidden")){
+            $(this).addClass("bg-color");
+            $('.inner-header').next(".navigation:visible").slideUp(400).prev().removeClass("bg-color");
+            $(this).next(".navigation").slideDown(400);
+        }
         
-    })
+    });
+     //stoper la propagation
+    $(".container-main-menu").on("click",(e)=>{
+    e.stopPropagation();
+    });
+    //outSideClick
+        $("html").on("click",()=>{
+        $(".inner-header").next(".navigation").slideUp(400).slideUp(400).prev().removeClass("bg-color");
+    });
 
 
 });
